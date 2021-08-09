f2fa70e3ed627e6d2cea620f382e654d41c29c1b
Ulrich Gratz <ulrichgratz@timocom.com>
2021-08-09 15:32:26 +0200

    add binary gap

---
+++ binary_gap/src/main/java/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+    public  int solution(int n) {
+
+        String binaryString = Integer.toBinaryString(n);
+
+        // your code goes here
+
+        return 0;
+    }
+
+}
---
+++ binary_gap/src/test/java/SolutionTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void solution() {
+    }
+}
